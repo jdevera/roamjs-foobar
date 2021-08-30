dd13ae9768ce6d7393d44d41c2922dde710d49a3
Jacobo de Vera <devel@jacobodevera.com>
2021-08-30 22:46:57 +0100

    Export ID to write simple test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { toConfig, runExtension } from "roam-client";
 import { createConfigObserver } from "roamjs-components";
 
-const ID = "roamjs-foobar";
+export const ID = "roamjs-foobar";
 const CONFIG = toConfig(ID);
 runExtension(ID, () => {
   createConfigObserver({ title: CONFIG, config: { tabs: [] } });
